a60b3a84384224b74b4b0226ecf7e51bd354785a
monsmain <matinfarmani20@gmail.com>
2025-07-12 12:59:28 +0330

    Update scan.go

--- scan.go
+++ scan.go
@@ -145,7 +145,8 @@ func main() {
 	fmt.Println("Step 1 Complete. Best IPs found.")
 	fmt.Println("\nStep 2: Scanning ports on best IPs (TCP & UDP)...")
 
-	portsToScan := []int{443, 2408, 500, 1701, 4500, 8886, 908, 8854, 878, 4198, 955, 988, 3854, 894, 7156, 1074, 2371, 939, 864, 854, 1070, 3476, 1387, 7559, 890, 1018}
+        tcpPorts := []int{443, 8886, 908, 8854, 4198, 955, 988, 3854, 894, 7156, 1074, 939, 864, 854, 1070, 3476, 1387, 7559, 890, 1018}
+        udpPorts := []int{500, 1701, 4500, 2408, 878, 2371}
 	var portWg sync.WaitGroup
 	endpointResultsChan := make(chan EndpointResult, len(bestIPs)*len(portsToScan)*2)
 
